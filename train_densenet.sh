3675f82d4b5747f1d7c5c12a0eae7b85eb256895
lkct <liangkct@yahoo.com>
2018-07-02 13:39:54 +0800

    ...

--- train_densenet.sh
+++ train_densenet.sh
@@ -4,4 +4,3 @@ set -e
 
 python -u train_densenet.py --depth 76 --batch-size 256 --num-examples 50000 --gpus 0 --workspace 1024 \
     --lr 0.1 --wd 0.0001 --num-classes 10 # --model-load-epoch 0 --retrain
-# depth 112 oom, (no memonger)
